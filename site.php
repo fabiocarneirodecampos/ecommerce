ffe20935794073a59bd2eac82a59aedcc2793f06
Fabio Carneiro de Campos <43301886+fabiocarneirodecampos@users.noreply.github.com>
2019-02-02 19:01:17 -0200

    Add files via upload

--- site.php
+++ site.php
@@ -67,7 +67,55 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+	]);
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->addProduct($Product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($Product);
+
+	header("Location: /cart");
+	exit;
+	
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($Product, true);
+
+	header("Location: /cart");
+	exit;
+	
 });
 
 
